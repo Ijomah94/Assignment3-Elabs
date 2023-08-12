03ed7b4bb0a4cdc3876b23c1f51d8476412bc530
Ijomah94 <135565696+Ijomah94@users.noreply.github.com>
2023-08-12 21:13:03 +0100

    Add files via upload

---
+++ Assignment 3/index.js
@@ -0,0 +1,43 @@
+document.querySelectorAll("li")[0].style.color='red';
+document.querySelectorAll("li")[1].style.background='yellow';
+
+document.querySelectorAll("h1")[0].style.textTransform='uppercase';
+document.querySelectorAll("h1")[0].style.color='white';
+document.querySelectorAll("h1")[0].style.paddingLeft='30px';
+
+document.querySelectorAll("h2")[0].style.padding='30px';
+
+
+document.querySelectorAll("ul")[0].style.listStyleType='button';
+
+document.querySelectorAll("section")[0].style.background="green";
+
+document.querySelectorAll("section")[1].style.background="pink";
+document.querySelectorAll("section")[1].innerText="I WAS CHANGED BY THIS CODER USING JAVA SCRIPT DOM MANIPULATION";
+document.querySelectorAll("section")[1].style.padding="5rem";
+document.querySelectorAll("section")[1].style.fontFamily="cursive";
+
+
+document.querySelectorAll("section")[1].style.marginTop="20px";
+
+//section 3 Dom Manipulation.....................................................................
+document.querySelectorAll("section")[2].style.fontFamily="cursive";
+document.querySelectorAll("section")[2].style.background="skyblue";
+document.querySelectorAll("section")[2].style.border="2px solid black";
+document.querySelectorAll("section")[2].style.padding="20px";
+
+//first div inside section 3
+document.querySelectorAll('div')[0].style.background='orangered';
+document.querySelectorAll('div')[0].style.transform='rotate(90deg)';
+
+//second div inside section 3 DOM Manipulations
+document.querySelectorAll('div')[1].style.background='pink';
+document.querySelectorAll('div')[1].style.color='skyblue';
+document.querySelectorAll('div')[1].style.fontWeight='900';
+
+//third div inside section 3 DOM Manipulations
+document.querySelectorAll('div')[2].style.background='black';
+document.querySelectorAll('div')[2].style.color='orange';
+
+
+document.querySelectorAll("p")[2].style.color='green';
